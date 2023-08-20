72d0aa16741ef1dcab1f0f8d18e253f6fc27e0cd
mehdi <dehghani_m@rocketmail.com>
2023-08-20 20:35:33 +0330

    try make adapter films

---
+++ app/src/main/java/com/example/batmanproject/adapter/FilmsAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.batmanproject.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.batmanproject.databinding.ItemFilmsBinding
+import com.example.batmanproject.model.Search
+
+
+interface OnItemClickCallback {
+    fun onItemClick(symbol: String)
+}
+class FilmsAdapter(private val onItemClickCallback: OnItemClickCallback) : RecyclerView.Adapter<FilmsAdapter.FilmsViewHolder>() {
+
+
+    private val search: ArrayList<Search> = arrayListOf()
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmsViewHolder {
+        val binding = ItemFilmsBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return FilmsViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return search.size
+    }
+
+    override fun onBindViewHolder(holder: FilmsViewHolder, position: Int) {
+        holder.bind(search[position],onItemClickCallback)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateList(list: List<Search>) {
+        this.search.clear()
+        this.search.addAll(list)
+        notifyDataSetChanged()
+    }
+
+
+
+
+    inner class FilmsViewHolder(private val binding: ItemFilmsBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(model: Search,onItemClickCallback: OnItemClickCallback) {
+
+            itemView.setOnClickListener {
+                onItemClickCallback.onItemClick(
+                    model.imdbID
+                )
+            }
+        }
+    }
+}
